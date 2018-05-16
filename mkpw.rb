815408df1b609f5c59e4ac1b4d509ef3ffe664dd
Christian Kröger <ck@tfmt.de>
2018-05-17 00:13:29 +0200

    updated brew formula

--- mkpw.rb
+++ mkpw.rb
@@ -1,13 +1,12 @@
 class Mkpw < Formula
   desc "mkpw is a small tool to generate safe passwords"
   homepage "https://github.com/commx/mkpw"
-  url "https://github.com/commx/mkpw/archive/0.1.1.tar.gz"
-  version "0.1.1"
-  sha256 "b1f6b823427eee5cc5dbd09724aa0bed3ac6d33e59108a2cc52698a254279df6"
+  url "https://github.com/commx/mkpw/archive/0.2.0.tar.gz"
+  version "0.2.0"
+  sha256 "1bdf2023da767ac5f1735b7c4a2e3d1854eb0bedf6df72ce10c380567d7cd85e"
 
   def install
     bin.install "mkpw"
-    man8.install "mkpw.8"
   end
 
   test do
